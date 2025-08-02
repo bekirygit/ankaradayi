86841287532b8180ebefb8a341321c8825d13ec1
bekirygit <bekirygit1426@gmail.com>
2025-08-02 04:17:39 +0300

    Add WhiteNoise and configure for static files

--- sube_yonetim/settings.py
+++ sube_yonetim/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", # Yeni eklendi
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
